7f320e305f99b1585eb2e7596cfa39cf33704e9b
Enzo <enzo.gouge@epitech.eu>
2026-01-30 11:42:47 +0100

    feat: initialize kords-app with Next.js, Tailwind CSS, and backend config

---
+++ kords-api/src/middleware/auth.ts
@@ -0,0 +1,16 @@
+
+import jwt from 'jsonwebtoken';
+import { prisma } from '../routes/index'
+import { env } from 'prisma/config';
+export const authenticate = async (req: Request, res: Response, next: Function) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    const decoded = jwt.verify(token, env("JWT_SECRET"));
+
+    const user = await prisma.user.findUnique({
+        where: { id: decoded.userId },
+        select: { id: true, email: true, role: true }
+    });
+
+    req.user = user;
+    next();
+};
---
+++ kords-api/src/routes/index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient, Prisma } from '../../generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import express from 'express'
+
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+export const prisma = new PrismaClient({ adapter: pool })
+
+const app = express()
+
+app.use(express.json())
+
+// ... vos routes API REST iront ici
+
+// app.listen(5558, () =>
+//   console.log('REST API server ready at: http://localhost:5558'),
+// )
