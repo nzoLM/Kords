a9efb629332310367497a36ecea6ba9dfbf88e35
nzoLM <enzo.gouge@epitech.eu>
2026-04-30 17:05:44 +0200

    refactor: homepage layout

--- kords-app/next.config.mjs
+++ kords-app/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
